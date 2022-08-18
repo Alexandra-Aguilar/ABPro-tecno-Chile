9ea15872c387ea5fe07904807b776c4eedf40fb9
Fabian Torres <fabi.ignacio1998@gmail.com>
2022-08-18 15:58:49 -0400

    se comenta datosPosts, seria la data utilizada antes de realizar el fetch

--- assets/js/blogs.js
+++ assets/js/blogs.js
@@ -49,39 +49,39 @@ function showPage() {
 
 
 
-var datosPosts = [
-    {
-        "userId": 1,
-        "id": 1,
-        "title": "sunt aut facere repellat provident occaecati excepturi optio reprehenderit",
-        "body": "quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto"
-    },
+// var datosPosts = [
+//     {
+//         "userId": 1,
+//         "id": 1,
+//         "title": "sunt aut facere repellat provident occaecati excepturi optio reprehenderit",
+//         "body": "quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto"
+//     },
 
-    {
-        "userId": 1,
-        "id": 2,
-        "title": "qui est esse",
-        "body": "est rerum tempore vitae\nsequi sint nihil reprehenderit dolor beatae ea dolores neque\nfugiatblanditiis voluptate porro vel nihil molestiae ut reiciendis\nqui aperiam non debitis possimus qui neque nisi nulla"
-    },
-    {
-        "userId": 1,
-        "id": 3,
-        "title": "ea molestias quasi exercitationem repellat qui ipsa sit aut",
-        "body": "et iusto sed quo iure\nvoluptatem occaecati omnis eligendi aut ad\nvoluptatem doloribus velaccusantium quis pariatur\nmolestiae porro eius odio et labore et velit aut"
-    },
-    {
-        "userId": 1,
-        "id": 4,
-        "title": "eum et est occaecati",
-        "body": "ullam et saepe reiciendis voluptatem adipisci\nsit amet autem assumenda provident rerum culpa\nquishic commodi nesciunt rem tenetur doloremque ipsam iure\nquis sunt voluptatem rerum illo velit"
-    },
-    {
-        "userId": 1,
-        "id": 5,
-        "title": "nesciunt quas odio",
-        "body": "repudiandae veniam quaerat sunt sed\nalias aut fugiat sit autem sed est\nvoluptatem omnis possimusesse voluptatibus quis\nest aut tenetur dolor neque"
-    },
-]
+//     {
+//         "userId": 1,
+//         "id": 2,
+//         "title": "qui est esse",
+//         "body": "est rerum tempore vitae\nsequi sint nihil reprehenderit dolor beatae ea dolores neque\nfugiatblanditiis voluptate porro vel nihil molestiae ut reiciendis\nqui aperiam non debitis possimus qui neque nisi nulla"
+//     },
+//     {
+//         "userId": 1,
+//         "id": 3,
+//         "title": "ea molestias quasi exercitationem repellat qui ipsa sit aut",
+//         "body": "et iusto sed quo iure\nvoluptatem occaecati omnis eligendi aut ad\nvoluptatem doloribus velaccusantium quis pariatur\nmolestiae porro eius odio et labore et velit aut"
+//     },
+//     {
+//         "userId": 1,
+//         "id": 4,
+//         "title": "eum et est occaecati",
+//         "body": "ullam et saepe reiciendis voluptatem adipisci\nsit amet autem assumenda provident rerum culpa\nquishic commodi nesciunt rem tenetur doloremque ipsam iure\nquis sunt voluptatem rerum illo velit"
+//     },
+//     {
+//         "userId": 1,
+//         "id": 5,
+//         "title": "nesciunt quas odio",
+//         "body": "repudiandae veniam quaerat sunt sed\nalias aut fugiat sit autem sed est\nvoluptatem omnis possimusesse voluptatibus quis\nest aut tenetur dolor neque"
+//     },
+// ]
 
 /*datosPosts.forEach((post)  =>{
     var cardsposts = document.getElementById("cardsposts");
@@ -195,6 +195,8 @@ const recibirPromise = async () => { //La recibimos en el método asincrono
     const mensajePromise = await retornoPromise();
     console.log(mensajePromise);
 }
+
+
 
 
 const favoritos = []
